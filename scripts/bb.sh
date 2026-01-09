f96334d69c9ff7c7ab542ac04b5065951d11b7a8
Geoff White <l0r3zz@gmail.com>
2026-01-09 00:12:04 -0800

    don't save bb.sh

--- scripts/bb.sh
+++ scripts/bb.sh
@@ -1,4 +1,7 @@
 # make sure you export MD_PATH, PDF_PATH, OUTPUT_DIR 
+export MD_PATH=/Users/geoffwhite/Desktop/Dropbox/GEN-AI/SLO-BLACKSWAN/sloblackswan-repo/SLOBLACKSWAN-v0.xx/SLOBLACKSWAN-v0.xx.md
+export PDF_PATH=/Users/geoffwhite/Desktop/Dropbox/GEN-AI/SLO-BLACKSWAN/sloblackswan-repo/SLOBLACKSWAN-v0.xx/SLOBLACKSWAN-v0.xx.pdf
+export OUTPUT_DIR=/Users/geoffwhite/Desktop/Dropbox/GEN-AI/SLO-BLACKSWAN/sloblackswan-repo/SLOBLACKSWAN-v0.xx
 python scripts/build_book.py -s content -d SLOBLACKSWAN -v "v0.xx" -m MANIFEST.md
 python scripts/insert_pagebreaks.py --overwrite $MD_PATH
 doctoc ${MD_PATH} --title "## Table of Contents" --maxlevel 2
